43f05136f50403482ce9ac9c2443f271e6f557e0
huweidong <466463054@qq.com>
2019-02-13 16:25:31 +0800

    35435210

---
+++ app/src/main/java/com/jingna/shopapp/app/MyApplication.java
@@ -0,0 +1,57 @@
+package com.jingna.shopapp.app;
+
+import android.app.Activity;
+import android.app.Application;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2019/2/13.
+ */
+
+public class MyApplication extends Application {
+
+    private static MyApplication instance;
+    private List<Activity> mList = new LinkedList<Activity>();
+
+    public MyApplication() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+
+    public synchronized static MyApplication getInstance() {
+        if (null == instance) {
+            instance = new MyApplication();
+        }
+        return instance;
+    }
+
+    // add Activity
+    public void addActivity(Activity activity) {
+        mList.add(activity);
+    }
+
+    public void exit() {
+        try {
+            for (Activity activity : mList) {
+                if (activity != null)
+                    activity.finish();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.exit(0);
+        }
+    }
+
+    public void onLowMemory() {
+        super.onLowMemory();
+        System.gc();
+    }
+
+}
