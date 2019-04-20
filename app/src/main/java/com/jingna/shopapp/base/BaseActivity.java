0897e4e279355a7939238271b3616e744df1f4bd
huweidong <466463054@qq.com>
2019-04-20 09:29:24 +0800

    2134234

--- app/src/main/java/com/jingna/shopapp/base/BaseActivity.java
+++ app/src/main/java/com/jingna/shopapp/base/BaseActivity.java
@@ -13,6 +13,12 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
+import com.youth.banner.Banner;
+import com.youth.banner.BannerConfig;
+import com.youth.banner.Transformer;
+
+import java.util.List;
+
 public class BaseActivity extends AppCompatActivity {
 ;
     @Override
@@ -51,6 +57,29 @@ public class BaseActivity extends AppCompatActivity {
         normalDialog.setNegativeButton("关闭", listener1);
         // 显示
         normalDialog.show();
+    }
+
+    public void init(Banner banner, List<String> images) {
+
+        //设置banner样式
+        banner.setBannerStyle(BannerConfig.CIRCLE_INDICATOR);
+        //设置图片加载器
+        banner.setImageLoader(new GlideImageLoader());
+        //设置图片集合
+        banner.setImages(images);
+        //设置banner动画效果
+        banner.setBannerAnimation(Transformer.DepthPage);
+        //设置标题集合（当banner样式有显示title时）
+//        banner.setBannerTitles(titles);
+        //设置自动轮播，默认为true
+        banner.isAutoPlay(true);
+        //设置轮播时间
+        banner.setDelayTime(5000);
+        //设置指示器位置（当banner模式中有指示器时）
+        banner.setIndicatorGravity(BannerConfig.CENTER);
+        //banner设置方法全部调用完毕时最后调用
+        banner.start();
+
     }
 
     @Override
